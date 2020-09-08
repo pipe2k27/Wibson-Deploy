c0c82d59351ac403d122a717c921bbfdce9e1276
felipe <feliperarnau@gmail.com>
2020-09-08 17:19:43 -0300

    add nerio

--- src/components/AboutUs/components/Team/TeamList.js
+++ src/components/AboutUs/components/Team/TeamList.js
@@ -2,7 +2,7 @@ export let teamList = [
   { name: "Rodrigo Irarrazaval", role: "CEO", pic: "/images/ro.png" },
   { name: "Daniel Fernandez", role: "CTO", pic: "/images/dani.png" },
   { name: "Adrian Ertorteguy", role: "Operations", pic: "/images/adri.png" },
-  { name: "Nerio Ramirez", role: "Developer", pic: "/images/dani.png" },
-  { name: "Felipe Perarnau", role: "Developer", pic: "/images/toro.JPG" },
-  { name: "Pablo Torrubiano", role: "Developer", pic: "/images/pablo.JPG" },
+  { name: "Nerio Ramirez", role: "Developer", pic: "/images/nerio.jpg" },
+  { name: "Felipe Perarnau", role: "Developer", pic: "/images/toro.jpg" },
+  { name: "Pablo Torrubiano", role: "Developer", pic: "/images/pablo.jpg" },
 ];
