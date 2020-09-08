4743b0ff6cbbfed9ba0c843325170433214e1d4c
felipe <feliperarnau@gmail.com>
2020-09-08 16:32:04 -0300

    Updates

---
+++ precache-manifest.31b443157bfaf9f49680aeec308b14cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2f193a43533ba6cb32ed1b682f8cc5",
+    "url": "/Wibson-Deploy/index.html"
+  },
+  {
+    "revision": "84140f9f4032ca6ef22a",
+    "url": "/Wibson-Deploy/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "c9bb5fe9b121c9b43cee",
+    "url": "/Wibson-Deploy/static/css/main.3c5831f6.chunk.css"
+  },
+  {
+    "revision": "84140f9f4032ca6ef22a",
+    "url": "/Wibson-Deploy/static/js/2.7ed4ecb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wibson-Deploy/static/js/2.7ed4ecb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bb5fe9b121c9b43cee",
+    "url": "/Wibson-Deploy/static/js/main.1a1e7371.chunk.js"
+  },
+  {
+    "revision": "7310bd802712415ab9c0",
+    "url": "/Wibson-Deploy/static/js/runtime-main.be336239.js"
+  }
+]);
